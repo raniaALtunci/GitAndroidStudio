92c1a5c3ca7cca39f557f6ad650dcf5bd5f8f22e
raniaALtunci <rania.altunci@gmail.com>
2019-01-06 23:45:32 +0300

    xml file commit part2

--- app/src/main/java/com/example/hatim/gitandroidstudio/MainActivity.java
+++ app/src/main/java/com/example/hatim/gitandroidstudio/MainActivity.java
@@ -12,6 +12,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Log.i("TAG","commit mdry");
         Log.i("TAG","commit mdry");
+        Log.i("TAG","msgy");
 
     }
 }
